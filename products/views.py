0296afe9a060dd8b3d3913e03cea77d7b4013470
Usenovaa <usenova.a00@gmail.com>
2024-02-20 12:33:02 +0600

    products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Product
+from .serializers import ProductListSerializer, ProductSerializer, CategorySerializer
+from rest_framework import generics
 
-# Create your views here.
+# class CategoryView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+
+# class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+    
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        return self.serializer_class
+        
+
+
+
+
+
+
